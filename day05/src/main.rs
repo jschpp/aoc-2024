81b1f297b437fc70e6e0ea2fa5c138f9ff80adb7
Johannes Schöpp <darkdiscover@gmail.com>
2024-12-12 15:18:19 +0100

    cleanup day05

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,15 +1,21 @@
 use std::{cmp::Ordering, collections::HashMap};
 
-use nom::{bytes::complete::tag, character::complete::{self, line_ending}, multi::{many1, separated_list1}, sequence::{separated_pair, terminated}, IResult};
+use nom::{
+    bytes::complete::tag,
+    character::complete::{self, line_ending},
+    multi::separated_list1,
+    sequence::separated_pair,
+    IResult,
+};
 
 #[derive(Debug, Default)]
 struct SpecialNumberOrdering {
-    order: HashMap<(u64,u64), Ordering>
+    order: HashMap<(u64, u64), Ordering>,
 }
 
 impl SpecialNumberOrdering {
     fn cmp(&self, lhs: u64, rhs: u64) -> Ordering {
-        match self.order.get(&(lhs,rhs)) {
+        match self.order.get(&(lhs, rhs)) {
             Some(ord) => *ord,
             None => Ordering::Equal,
         }
@@ -20,8 +26,8 @@ impl From<Vec<(u64, u64)>> for SpecialNumberOrdering {
     fn from(value: Vec<(u64, u64)>) -> Self {
         let mut ord = SpecialNumberOrdering::default();
         for pair in value {
-            ord.order.insert((pair.0,pair.1), Ordering::Less);
-            ord.order.insert((pair.1,pair.0), Ordering::Greater);
+            ord.order.insert((pair.0, pair.1), Ordering::Less);
+            ord.order.insert((pair.1, pair.0), Ordering::Greater);
         }
         ord
     }
@@ -35,34 +41,40 @@ fn parse_input(input: &str) -> IResult<&str, (SpecialNumberOrdering, Vec<Vec<u64
     let (input, pairs) = separated_list1(line_ending, parse_ordering)(input)?;
     let (input, _) = line_ending(input)?;
     let (input, _) = line_ending(input)?;
-    let (input, orders) = separated_list1(line_ending,separated_list1(tag(","), complete::u64))(input)?;
+    let (input, orders) =
+        separated_list1(line_ending, separated_list1(tag(","), complete::u64))(input)?;
     let special_ordering = SpecialNumberOrdering::from(pairs);
     Ok((input, (special_ordering, orders)))
 }
 
-
 fn main() {
     let input = include_str!("../input.txt");
-    let (_,(ord, orders)) = parse_input(input).unwrap();
-    let middle_sum: u64 = orders.iter().flat_map(|a| {
-        let mut b = a.clone();
-        b.sort_by(|a,b| ord.cmp(*a, *b));
-        if *a == b {
-            Some(a[a.len() / 2])
-        } else {
-            None
-        }
-    }).sum();
+    let (_, (ord, orders)) = parse_input(input).unwrap();
+    let middle_sum: u64 = orders
+        .iter()
+        .flat_map(|a| {
+            let mut b = a.clone();
+            b.sort_by(|a, b| ord.cmp(*a, *b));
+            if *a == b {
+                Some(a[a.len() / 2])
+            } else {
+                None
+            }
+        })
+        .sum();
     println!("part1: {middle_sum}");
 
-    let middle_sum: u64 = orders.iter().flat_map(|a| {
-        let mut b = a.clone();
-        b.sort_by(|a,b| ord.cmp(*a, *b));
-        if *a == b {
-            None
-        } else {
-            Some(b[b.len() / 2])
-        }
-    }).sum();
+    let middle_sum: u64 = orders
+        .iter()
+        .flat_map(|a| {
+            let mut b = a.clone();
+            b.sort_by(|a, b| ord.cmp(*a, *b));
+            if *a == b {
+                None
+            } else {
+                Some(b[b.len() / 2])
+            }
+        })
+        .sum();
     println!("part2: {middle_sum}");
 }
