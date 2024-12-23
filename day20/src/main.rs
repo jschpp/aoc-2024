a47d30e042dbdbec32d22c593345a878c001e631
Johannes Schöpp <darkdiscover@gmail.com>
2024-12-23 16:11:42 +0100

    added day20 part2

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -10,8 +10,11 @@ fn main() {
     let input = include_str!("../input.txt");
     let g = parse(input);
 
-    let part1_result = part1(g);
+    let part1_result = part1(&g);
     println!("part1: {part1_result}");
+
+    let part2_result = part2(&g);
+    println!("part2: {part2_result}");
 }
 
 #[derive(Debug, Clone)]
@@ -28,8 +31,8 @@ enum Cell {
     Wall,
 }
 
-fn part1(track: RaceTrack) -> usize {
-    let original = search_path(&track);
+fn part1(track: &RaceTrack) -> usize {
+    let original = search_path(track);
     let pairs: Vec<(Point, Point)> = track
         .grid
         .indexed_iter()
@@ -132,4 +135,79 @@ fn parse(input: &str) -> RaceTrack {
         }
     }
     RaceTrack { grid, start, end }
+}
+
+fn get_distances(track: &RaceTrack) -> Grid<Option<i32>> {
+    let mut dists: Grid<Option<i32>> = Grid::new(track.grid.rows(), track.grid.cols());
+    let mut current_position = track.start;
+    dists[current_position] = Some(0);
+    while current_position != track.end {
+        for (neighbour, _) in neighbours(&current_position, &track.grid) {
+            if dists[neighbour].is_none() {
+                dists[neighbour] = Some(dists[current_position].unwrap() + 1);
+                current_position = neighbour;
+            }
+        }
+    }
+    dists
+}
+
+fn part2(track: &RaceTrack) -> usize {
+    let mut count = 0_usize;
+    let dist = get_distances(track);
+    for ((r, c), _) in track.grid.indexed_iter().filter(|(_, c)| **c != Cell::Wall) {
+        let p = Point(r, c);
+
+        for radius in 2..=20 {
+            for delta_row in 0..=radius {
+                let delta_col = radius - delta_row;
+                let options: HashSet<_> = checked_surroundings(p, delta_row, delta_col)
+                    .into_iter()
+                    .filter(|x| {
+                        // bounds check and checking if its a wall
+                        x.0 < track.grid.rows()
+                            && x.1 < track.grid.cols()
+                            && track.grid[x] != Cell::Wall
+                    })
+                    .collect();
+                for next in options {
+                    // we need to save at least 100 picoseconds + the cost of going to next itself (which is the radius)
+                    if dist[p].unwrap() - dist[next].unwrap() >= 100 + radius {
+                        count += 1;
+                    }
+                }
+            }
+        }
+    }
+    count
+}
+
+#[inline(always)]
+fn checked_surroundings(p: Point, delta_row: i32, delta_col: i32) -> HashSet<Point> {
+    [
+        (delta_row, delta_col),
+        (delta_row, -delta_col),
+        (-delta_row, delta_col),
+        (-delta_row, -delta_col),
+    ]
+    .into_iter()
+    .flat_map(|x| p + x)
+    .collect()
+}
+
+// Used for debugging distance grid
+#[allow(dead_code)]
+fn print_grid2(g: &Grid<Option<i32>>) {
+    for row in g.iter_rows() {
+        for c in row {
+            print!(
+                "{}",
+                match c {
+                    Some(val) => format!("{val:3}"),
+                    None => "   ".to_owned(),
+                }
+            );
+        }
+        println!();
+    }
 }
